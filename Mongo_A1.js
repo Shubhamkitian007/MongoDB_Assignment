23fa9828e58a87b82167a2b6ec40883011ba0c2f
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-08-01 17:57:37 +0530

    Add Pretty()

--- Mongo_A1.js
+++ Mongo_A1.js
@@ -71,3 +71,69 @@ switched to db Human_Resourse
                 ObjectId("62e7c41d363005bc114376da")
         ]
 }
+
+//  Display Documents
+
+db.employee.find().pretty()
+{
+        "_id" : ObjectId("62e7c41d363005bc114376d5"),
+        "firstName" : "Shubham",
+        "lastName" : "Kumar",
+        "Salary" : 35500,
+        "department" : "HR",
+        "overallExp" : 2,
+        "contactInfo" : 5533226614,
+        "yearGrade" : 2022,
+        "Address" : "Jamui"
+}
+{
+        "_id" : ObjectId("62e7c41d363005bc114376d6"),
+        "firstName" : "Milan",
+        "lastName" : "Kumar",
+        "Salary" : 150000,
+        "department" : "Sell",
+        "overallExp" : 5,
+        "contactInfo" : 3221515667,
+        "yearGrade" : 2018,
+        "Address" : "Patna"
+}
+{
+        "_id" : ObjectId("62e7c41d363005bc114376d7"),
+        "firstName" : "Prachi",
+        "lastName" : "Singh",
+        "department" : "Class XI",
+        "contactInfo" : 8866224455,
+        "Address" : "Laxmipur"
+}
+{
+        "_id" : ObjectId("62e7c41d363005bc114376d8"),
+        "firstName" : "Surbhi",
+        "lastName" : "Kumari",
+        "Salary" : 500000,
+        "Department" : "B.A Hons",
+        "contactInfo" : 2022,
+        "yearGrade" : 2022,
+        "Address" : "Deoghar"
+}
+{
+        "_id" : ObjectId("62e7c41d363005bc114376d9"),
+        "firstName" : "Sharad",
+        "lastName" : "Kumar",
+        "salary" : "65000",
+        "department" : "Teacher",
+        "overallExp" : 5,
+        "contactInfo" : 4563214789,
+        "yearGrade" : 2017,
+        "Address" : "Dhanbad"
+}
+{
+        "_id" : ObjectId("62e7c41d363005bc114376da"),
+        "firstName" : "Rohit",
+        "lastName" : "Gupta",
+        "salary" : 450000,
+        "department" : "Mining",
+        "contactInfo" : 5588226644,
+        "yearGrade" : 2014,
+        "overallExp" : 8,
+        "Address" : "Ranchi"
+}
